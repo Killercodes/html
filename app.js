f2b64651bfe553fb6658dedfb85701974a34deca
KILLER.CODES <Killercodes@users.noreply.github.com>
2022-06-13 11:42:38 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-if('serviceWorker' in navigator){
+if('app-worker' in navigator){
       navigator.serviceWorker.register("/app-worker.js")
       .then(registeration => {
         console.log("SW registered");
