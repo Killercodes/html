64051cbbada7317d04c770ce9ab8d44efb379acf
KILLER.CODES <Killercodes@users.noreply.github.com>
2022-06-13 12:34:13 +0530

    Update app-worker.js

--- app-worker.js
+++ app-worker.js
@@ -1,7 +1,7 @@
 self.addEventListener("install", e =>{
     e.waitUntil(
         caches.open("static").then(cache =>{
-            return cache.addAll(['./','./logo.png']);
+            return cache.addAll(['./','/logo.png']);
         })
     );
 });
